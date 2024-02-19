6520a35112ee3f362f006fd6ccca153baae834ae
Zachary Carter <carterza@gmail.com>
2024-02-19 08:43:29 -0600

    removing riot API key from config

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-config :blitzurgist, :magic_word, "RGAPI-a29943a1-7976-4df4-bba3-09755d189c60"
+config :blitzurgist, :magic_word, "Abracadabra"
 
 config :blitzurgist, :match_api_base_url, "api.riotgames.com/lol/match/v5/matches"
 config :blitzurgist, :summoner_api_base_url, "api.riotgames.com/lol/summoner/v4/summoners"
